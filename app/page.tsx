b37dd7deca1df63a3fed49697aab7ff328f979b0
ES, Choi <es.choi@modulabs.co.kr>
2025-06-12 00:21:19 +0900

    create: 프로젝트 초기세팅

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Hello World</h1>
+      <p>This is a test</p>
+
+    </div>
+  );
+}
