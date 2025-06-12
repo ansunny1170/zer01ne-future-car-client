9f7c85a51b702611e92589108a3c54cab797981c
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-06-12 23:50:05 +0900

    feat: 중앙화면 버튼 위치 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import VideoPlayer from "./components/video-player";
+import Speech from "./components/speech";
 
 export default function Home() {
   const options = ["a", "b", "c"];
@@ -35,37 +36,44 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen cursor-none123 overflow-hidden">
+    <div className="flex flex-col items-start justify-center text-left h-screen cursor-none123 overflow-hidden">
       <VideoPlayer message={message || "a"} direction="center" />
-      <div className="text-center p-4">
-          <h1 className="text-4xl font-bold text-red-600 mb-4">Center Page</h1>
-          <p className="text-gray-600">{message}</p>
+
+      <div className="pl-8">
+        <div className="pb-4">
+            <h1 className="text-4xl font-bold text-red-600 mb-4">Center Page</h1>
+            <p className="text-gray-600">{message}</p>
+        </div>
+
+        <div className="
+          flex flex-row gap-4 
+          *:p-2 *:bg-gray-200 *:text-gray-800 *:rounded-md *:transition-all *:duration-500 *:cursor-none123
+          *:hover:bg-gray-400 *:hover:scale-110 *:hover:text-white *:hover:font-bold
+          *:focus:bg-gray-400 *:focus:scale-110 *:focus:text-white *:focus:font-bold *:focus:outline-none
+        ">
+          <button
+            className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
+            onClick={() => handleClick("a")}
+          >
+            A
+          </button>
+          <button
+            className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
+            onClick={() => handleClick("b")}
+          >
+            B
+          </button>
+          <button
+            className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
+            onClick={() => handleClick("c")}
+          >
+            C
+          </button>
+        </div>
       </div>
 
-      <div className="
-        flex flex-row gap-4 
-        *:p-2 *:bg-gray-200 *:text-gray-800 *:rounded-md *:transition-all *:duration-500 *:cursor-none123
-        *:hover:bg-gray-400 *:hover:scale-110 *:hover:text-white *:hover:font-bold
-        *:focus:bg-gray-400 *:focus:scale-110 *:focus:text-white *:focus:font-bold *:focus:outline-none
-      ">
-        <button
-          className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
-          onClick={() => handleClick("a")}
-        >
-          A
-        </button>
-        <button
-          className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
-          onClick={() => handleClick("b")}
-        >
-          B
-        </button>
-        <button
-          className={`w-20 h-20 rounded-full bg-white/80 backdrop-blur-sm`}
-          onClick={() => handleClick("c")}
-        >
-          C
-        </button>
+      <div className="">
+        <Speech />
       </div>
 
       <div className="hidden">
