af8da66f924d7fd87588cabf27e5a6f1060b1226
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-07-10 02:27:42 +0900

    wip

--- app/components/Steps/step0.tsx
+++ app/components/Steps/step0.tsx
@@ -30,14 +30,12 @@ export default function Step0() {
         <div className="relative rounded-[32px] p-[1.5px] bg-[linear-gradient(to_top,rgba(255,255,255,0.95)_0%,rgba(255,255,255,0.8)_10%,rgba(255,255,255,0.2)_40%,rgba(255,255,255,0.1)_60%,rgba(255,255,255,0.2)_80%)]">
           {/* 내용 + 배경 */}
           {/* 배경 블러 효과 */}
-      <div className="absolute inset-0 scale-[calc(100%+2px)] rounded-[32px] opacity-40 bg-[linear-gradient(to_right,#BB00FF_0%,#FF4D00_50%,#FF0033_100%)] mix-blend-color-dodge"></div>
+          
+          <div className="absolute inset-0 scale-[calc(100%+2px)] rounded-[32px] opacity-401 bg-[linear-gradient(to_right,#BB00FF_0%,#FF4D00_50%,#FF0033_100%)] [mask-image:linear-gradient(to_top,black_0%,transparent_50%)] mix-blend-color-dodge"></div>
 
-          <div className="animate-fade-in relative z-10 rounded-[31px] 1backdrop-blur-md px-8 py-6">
-            
+          <div className="animate-fade-in relative z-10 rounded-[31px] backdrop-blur-md px-8 py-6">
             <div className="text-white text-6xl font-bold mb-2">78%</div>
             <div className="text-white/80 text-xl">브레이크 패드 마모도</div>
-          
-            
           </div>
         </div>
       </div>
