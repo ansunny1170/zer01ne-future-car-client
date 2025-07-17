d67660adad13d93a5161088e2ef8bc451d8574a3
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-07-17 22:23:11 +0900

    feat: CORS에러 임시대처

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://15.165.63.35:8000/:path*', // 백엔드 주소
+      },
+    ]
+  },
 };
 
 export default nextConfig;
