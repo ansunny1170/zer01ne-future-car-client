1cc2ada0e1993bd541d2a4f90e0b1ea5ff3657ef
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-07-19 18:57:08 +0900

    feat: 기본 컴포넌트 퍼블리싱

---
+++ app/components/icons/cloud.tsx
@@ -0,0 +1,37 @@
+export default function CloudIcon() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_132_658" style={{maskType: "alpha"}} maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+            <rect width="24" height="24" fill="url(#paint0_linear_132_658)" fillOpacity="0.8"/>
+            </mask>
+            <g mask="url(#mask0_132_658)">
+            <g filter="url(#filter0_dd_132_658)">
+            <path d="M6.5 20C4.98333 20 3.6875 19.475 2.6125 18.425C1.5375 17.375 1 16.0917 1 14.575C1 13.275 1.39167 12.1167 2.175 11.1C2.95833 10.0833 3.98333 9.43333 5.25 9.15C5.66667 7.61667 6.5 6.375 7.75 5.425C9 4.475 10.4167 4 12 4C13.95 4 15.6042 4.67917 16.9625 6.0375C18.3208 7.39583 19 9.05 19 11C20.15 11.1333 21.1042 11.6292 21.8625 12.4875C22.6208 13.3458 23 14.35 23 15.5C23 16.75 22.5625 17.8125 21.6875 18.6875C20.8125 19.5625 19.75 20 18.5 20H6.5Z" fill="white"/>
+            </g>
+            </g>
+            <defs>
+            <filter id="filter0_dd_132_658" x="-26.135" y="-2.885" width="76.27" height="70.27" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="20.25"/>
+            <feGaussianBlur stdDeviation="13.5675"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.47 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_132_658"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="3.00375"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.29 0"/>
+            <feBlend mode="normal" in2="effect1_dropShadow_132_658" result="effect2_dropShadow_132_658"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_132_658" result="shape"/>
+            </filter>
+            <linearGradient id="paint0_linear_132_658" x1="12" y1="0" x2="12" y2="24" gradientUnits="userSpaceOnUse">
+            <stop stopColor="white"/>
+            <stop offset="1" stopColor="white" stopOpacity="0.7"/>
+            </linearGradient>
+            </defs>
+        </svg>
+
+    );
+}
--- app/components/ui/progress-box.tsx
+++ app/components/ui/progress-box.tsx
@@ -1,45 +1,65 @@
 import { getFormattedTime } from "@/app/utils";
 import BasicBox from "./basic-box";
+import CloudIcon from "../icons/cloud";
 
-
+interface ProgressBoxProps {
+  stop?: boolean;
+  progress?: number;
+}
 
 export default function ProgressBox({
+  stop = true,
   progress = 60
-}) {
+}: ProgressBoxProps) {
   const time = 14;
   const distance = 9;
   const speed = { kmh: 128, mph: 5 };
   
   return (
-    <BasicBox className="flex gap-[30px] items-center justify-between text-center">
-      <div className="leading-[1.2]">
-        <p className="text-[35px] font-bold">{time}min</p>
-        <p className="text-[17px] opacity-60">{distance}km {getFormattedTime()}</p>
+    <div className="flex flex-col gap-[10px]">
+      <div className="flex items-center gap-[4px]">
+        <CloudIcon />
+        <p className="text-white text-[23px] opacity-60 font-semibold">27.5 º</p>
       </div>
-
-      {/* 진행바 */}
-      <div className="h-[10px] rounded-full bg-black/20 grow w-[378px]">
-        <div 
-          className="relative h-full rounded-full bg-gradient-to-r from-[#FF4800] via-[#00FF85] to-[#FFE600]"
-          style={{ width: `${progress}%` }}
-        >
-          <p
-            className="absolute top-1/2 right-0 -translate-y-1/2 w-4 h-4 bg-white rounded-full shadow-[0_0_10px_rgba(255,255,255,0.5)]"
-          />
+      <BasicBox className="w-full flex gap-[30px] items-center justify-between text-center">
+        <div className="leading-[1.2] whitespace-nowrap">
+          {
+            !stop ? (
+              <p className="text-[17px] font-bold opacity-60">목적지가 없습니다.</p>
+            ) : (
+              <div className="">
+                <p className="text-[35px] font-bold">{time}min</p>
+                <p className="text-[17px] opacity-60">{distance}km {getFormattedTime()}</p>
+              </div>
+            )
+          }
+          
         </div>
-      </div>
 
-      <ul className="flex gap-[10px]">
-        <li className="leading-[1.2]">
-          <p className="text-[35px] font-bold">{speed.kmh}</p>
-          <p className="text-[22px] opacity-60">km/h</p>
-        </li>
-        <li className="leading-[1.2]">
-          <p className="text-[35px] font-bold">{speed.mph}</p>
-          <p className="text-[22px] opacity-60">MPH</p>
-        </li>
+        {/* 진행바 */}
+        <div className="h-[10px] rounded-full bg-black/20 grow min-w-[300px]">
+          <div 
+            className="relative h-full rounded-full bg-gradient-to-r from-[#FF4800] via-[#00FF85] to-[#FFE600]"
+            style={{ width: `${progress}%` }}
+          >
+            <p
+              className="absolute top-1/2 right-0 -translate-y-1/2 w-4 h-4 bg-white rounded-full shadow-[0_0_10px_rgba(255,255,255,0.5)]"
+            />
+          </div>
+        </div>
 
-      </ul>
-    </BasicBox>
+        <ul className="flex gap-[10px]">
+          <li className="leading-[1.2]">
+            <p className="text-[35px] font-bold">{speed.kmh}</p>
+            <p className="text-[22px] opacity-60">km/h</p>
+          </li>
+          <li className="leading-[1.2]">
+            <p className="text-[35px] font-bold">{speed.mph}</p>
+            <p className="text-[22px] opacity-60">MPH</p>
+          </li>
+
+        </ul>
+      </BasicBox>
+    </div>
   );
 } 
