484af17e7b085348631aacd04a6e9dbf86643f8a
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-08-14 20:50:09 +0900

    feat: api 교체

--- app/api/speech/index.ts
+++ app/api/speech/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { StepInfo } from '@/type';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://dev.ftcar.org',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
