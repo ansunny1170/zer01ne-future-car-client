5a68836ace4095972ef677d86225eb546b0f0028
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-08-17 20:22:04 +0900

    feat: svg case 수정

--- app/components/ui/usp-popup-wrapper.tsx
+++ app/components/ui/usp-popup-wrapper.tsx
@@ -18,9 +18,9 @@ export default function UspPopupWrapper({ data }: { data: { description: string 
                 <path d="M12.3688 0C12.3688 6.42242 17.5776 11.6288 24 11.6288V11.6288V12.3688V12.3688C17.5763 12.3688 12.3688 17.5763 12.3688 24V24H11.6288V24C11.6288 17.5776 6.42242 12.3688 0 12.3688V12.3688V11.6288V11.6288C6.42242 11.6288 11.6288 6.42242 11.6288 0V0H12.3688V0Z" fill="url(#paint0_linear_409_339)"/>
                 <defs>
                 <linearGradient id="paint0_linear_409_339" x1="24.8478" y1="0.991494" x2="4.33333" y2="20" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#4C8BFF"/>
-                <stop offset="0.721233" stop-color="#F7B094"/>
-                <stop offset="1" stop-color="#FFC73B"/>
+                <stop stopColor="#4C8BFF"/>
+                <stop offset="0.721233" stopColor="#F7B094"/>
+                <stop offset="1" stopColor="#FFC73B"/>
                 </linearGradient>
                 </defs>
               </svg>
