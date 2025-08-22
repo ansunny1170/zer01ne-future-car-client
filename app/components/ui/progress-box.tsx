183b49fb7e9d0baa5f8ba6743c676eacf37441d8
ES, Choi <69956156+escape1001@users.noreply.github.com>
2025-08-22 16:17:21 +0900

    feat: propgress box 컴포넌트 수정

--- app/components/ui/progress-box.tsx
+++ app/components/ui/progress-box.tsx
@@ -1,51 +1,44 @@
-import { getFormattedTime } from "@/utils";
+/* eslint-disable @next/next/no-img-element */
 import { useScene } from "@/context/scene-context";
 
 export default function ProgressBox() {
-  const time = 14;
-  const distance = 9;
-  const speed = { kmh: 128, mph: 5 };
   const {stepInfo} = useScene();
   const progress = Number(stepInfo?.step);
   const totalStep = 7;
   
   return (
-    <div className="flex gap-[30px] items-center justify-between text-center text-white">
-      <div className="leading-[1.2]">
-        <p className="text-[35px] font-bold">{time}min</p>
-        <p className="text-[17px] opacity-60">{distance}km {getFormattedTime()}</p>
-      </div>
+    <div className="flex gap-[48px] items-center justify-between text-center text-white">
+      <div className="opacity-90 flex">
+        <p className="flex items-center justify-center -translate-y-[48px]">
+          <img src="/assets/images/icon_handle.svg" alt="progress-box" className="w-[36px] h-[36px]" />
+        </p>
 
-      {/* 진행바 */}
-      <div className="h-[10px] rounded-full bg-black/40 grow w-[378px] relative">
-        <div 
-          className="h-full rounded-full relative transition-all duration-500 delay-100"
-          style={{ 
-            width: `${(progress || 1) / totalStep * 100}%`,
-            background: `linear-gradient(
-              90deg, 
-              #FF4800 0%, 
-              #00FF85 50%, 
-              #FFE600 100%
-            )`,
-            backgroundSize: '200% 100%',
-            // animation: 'gradientMove 2s ease infinite'
-          }}
-        >
-          <div
-            className="absolute top-1/2 right-0 -translate-y-1/2 w-4 h-4 bg-white rounded-full shadow-[0_0_10px_rgba(255,255,255,0.5)]"
-          />
+        <p className="text-[65px] font-semibold pl-[30px] pr-[20px]">68</p>
+
+        <div className="flex flex-col items-center justify-center leading-[1.4]">
+          <p className="text-[20px] text-[#FF3826] font-bold">100</p>
+          <p className="text-[16px] uppercase">km/h</p>
         </div>
       </div>
 
-      <ul className="flex gap-[10px]">
-        <li className="leading-[1.2]">
-          <p className="text-[35px] font-bold">{speed.kmh}</p>
-          <p className="text-[22px] opacity-60">km/h</p>
+      {/* 진행바 */}
+      <div className="h-[6px] rounded-full bg-black/40 grow w-[263px] relative">
+        <div 
+          className="h-full rounded-full relative transition-all duration-500 delay-100 bg-white"
+          style={{ 
+            width: `${(progress || 1) / totalStep * 100}%`
+          }}
+        />
+      </div>
+
+      <ul className="flex gap-[27px] opacity-90 uppercase">
+        <li className="leading-[1.4]">
+          <p className="text-[20px] font-bold">21</p>
+          <p className="text-[16px]">km</p>
         </li>
-        <li className="leading-[1.2]">
-          <p className="text-[35px] font-bold">{speed.mph}</p>
-          <p className="text-[22px] opacity-60">MPH</p>
+        <li className="leading-[1.4]">
+          <p className="text-[20px] font-bold">13:00</p>
+          <p className="text-[16px]">ETA</p>
         </li>
       </ul>
     </div>
